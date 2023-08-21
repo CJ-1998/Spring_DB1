06341c5e8135824db493ed4eed27f4edb988af93
CJ <choijun0627@konkuk.ac.kr>
2023-08-22 08:57:37 +0900

    <feat> 스프링 DB 1편 프로젝트 section 1
    JDBC 개발 등록
    
    등록할 객체인 Member 클래스 생성
    DB에 등록하는 MemberRepositoryV0 생성
    잘 등록되는지 확인하기 위한 MemberRepositoryV0Test 생성

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,17 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
+}
