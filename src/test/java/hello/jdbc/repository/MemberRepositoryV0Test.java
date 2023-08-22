934a076be145f4c358ca5e266a48064c3f31fb76
CJ <choijun0627@konkuk.ac.kr>
2023-08-22 15:12:15 +0900

    <feat> 스프링 DB 1편 프로젝트 section 1
    JDBC 개발 - 조회
    
    조회하는 기능을 만들기 위해 MemberRepositoryV0에 findById 함수 추가
    save로 저장한 member와 findById로 조회한 member 같은지 확인하는 테스트를
    MemberRepositoryv0Test에 추가

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,26 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
-    
+
     @Test
     void crud() throws SQLException {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
