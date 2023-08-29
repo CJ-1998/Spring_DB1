e854008b6c484c248779f6a6c1e725b1c038789d
CJ <choijun0627@konkuk.ac.kr>
2023-08-30 08:44:59 +0900

    <feat> 스프링 DB 1편 section 4
    트랜잭션 AOP 적용해서 서비스 로직에 트랜잭션 관련 코드 제거
    
    @Transactional을 붙여서 트랜잭션 AOP를 적용했다
    이 annotation을 보고 트랜잭션 프록시를 적용해준다
    
    트랜잭션 AOP를 적용한 MemberServiceV3_3 생성
    확인해보기 위해 MemberServiceV3_3Test 생성
    테스트에서 트랜잭션 AOP 적용하기 위해 스프링 띄우는 @SpringBootTest하고 의존 관계주입 받고 필요한 것들을 스프링 빈으로 등록했다.

--- src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV3_3Test.java
@@ -2,13 +2,20 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
@@ -16,30 +23,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
  */
 
-class MemberServiceV3_2Test {
+@Slf4j
+@SpringBootTest
+class MemberServiceV3_3Test {
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
-    private MemberRepositoryV3 memberRepository;
-    private MemberServiceV3_2 memberService;
-
-    @BeforeEach
-    void before() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-        memberRepository = new MemberRepositoryV3(dataSource);
-        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
-    }
+    @Autowired
+    MemberRepositoryV3 memberRepository;
+    @Autowired
+    MemberServiceV3_3 memberService;
 
     @AfterEach
     void after() throws SQLException {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+        @Bean
+        MemberServiceV3_3 memberServiceV3_3() {
+            return new MemberServiceV3_3(memberRepositoryV3());
+        }
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberRepository class={}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
     }
 
     @Test
@@ -78,7 +109,7 @@ class MemberServiceV3_2Test {
         //then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
-
+        
         //memberA의 돈이 롤백 되어야함
         assertThat(findMemberA.getMoney()).isEqualTo(10000);
         assertThat(findMemberEx.getMoney()).isEqualTo(10000);
